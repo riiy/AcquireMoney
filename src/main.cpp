801200271b33a2cc68c1baa1a5508ded5334b37c
riiy <zhangriiy@gmail.com>
2022-09-24 21:49:17 +0800

    add files

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
+#include <cpr/cpr.h>
 #include <iostream>
-int main()
-{
-	std::cout << "hello" << std::endl;
+#include <string>
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb,
+                            void *userp) {
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
+}
+
+int main(void) {
+  cpr::Response r = cpr::Get(
+      cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+      cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
+      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+  std::cout << r.text << std::endl;
+  return 0;
 }
