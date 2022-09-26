2f0122a5fb2fe0c525ed05a40205204ac65a6860
riiy <zhangriiy@gmail.com>
2022-09-26 22:19:52 +0800

    添加boost::program_options

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
 #include "eastmoney.h"
+#include <boost/program_options.hpp>
 #include <iostream>
 using namespace std;
+namespace po = boost::program_options;
 
+int main(int argc, const char *argv[]) {
+  po::options_description desc("Allowed options");
+  desc.add_options()("help", "produce help message")(
+      "stock,S", po::value<string>(), "查找stock");
 
-int main(void) {
-  auto stocks = get_stocks();
-  for (auto &[key, value] : stocks.items()) {
-    std::cout << key << " : " << value << "\n";
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    cout << desc << "\n";
+    return 1;
+  }
+
+  if (vm.count("stock")) {
+    auto stocks = get_stocks();
+    for (auto &[key, value] : stocks.items()) {
+      if (vm["stock"].as<string>() == key)
+        std::cout << key << " : " << value << "\n";
+    }
+  } else {
+    cout << "Compression level was not set.\n";
   }
   return 0;
 }
